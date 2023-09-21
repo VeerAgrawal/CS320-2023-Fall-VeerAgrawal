3e61eadba08eb665756bd71a74e78e38c32a6bcf
Veer Agrawal <veer@Veers-MBP.ht.home>
2023-09-20 23:35:07 -0400

    file

--- assigns/assign0/MySolution/assign0-3.ml
+++ assigns/assign0/MySolution/assign0-3.ml
@@ -1,7 +1,2 @@
-let rec int2str i =
-  if i < 0 then "-" ^ int2str (-i)
-  else if i < 10 then String.make 1 (Char.chr (48 + i))
-  else int2str (i / 10) ^ int2str (i mod 10);;
-
-(* let int2str(i0: int): string =
-  string_of_int i0;; *)
+let int2str(i0: int): string =
+  string_of_int i0;;
